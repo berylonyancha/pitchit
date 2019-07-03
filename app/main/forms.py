5a0499c3bca543a86796acab81982b049734088a
Beryl Onyancha <berylonyancha@gmail>
2019-07-03 14:10:18 +0300

    Create class to add pitches

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField,SelectField
 from wtforms.validators import Required
+
+class PitchForm(FlaskForm):
+    title = StringField("Pitch Title",validators=[Required()])
+    pitch = TextAreaField('Write your pitch here')
+    category = SelectField("Pitch category",choices=[('Pickup Lines','Pickup Lines'),('Interview Pitch','Interview Pitch'),('Product Pitch','Promotion Pitch')],validators=[Required()])
+    submit = SubmitField('Submit')
+
