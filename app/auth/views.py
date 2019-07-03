87f6844162c5a0c112ff87a3dca767a77ae228bc
Beryl Onyancha <berylonyancha@gmail>
2019-07-03 14:02:09 +0300

    Add register route

--- app/auth/views.py
+++ app/auth/views.py
@@ -20,3 +20,16 @@ def login():
     title ="Pitch Login"
     return render_template('auth/login.html',login_form=login_form,title=title)
 
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        mail_message("Welcome to Pitch","email/welcome_user",user.email,user=user)
+
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
