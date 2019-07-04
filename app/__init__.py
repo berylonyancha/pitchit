77de8da38b1d3ccf8d53e12d30d9fead3a95b423
Beryl Onyancha <berylonyancha@gmail>
2019-07-04 08:48:07 +0300

    Initialize the app and flask extension, register the blue print, configure uploadset

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,47 @@
-
 from flask import Flask
-from config import config_options
-from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from config import config_options
+from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
+from flask_simplemde import SimpleMDE
 
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+mail = Mail()
+simple = SimpleMDE()
 
-def create_app(config_state):
-    app = Flask(__name__)
-    app.config.from_object(config_options[config_state])
+photos = UploadSet('photos',IMAGES)
 
-
-    bootstrap.init_app(app)
-    db.init_app(app)
-
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+def create_app(config_name):
+    app=Flask(__name__)
 
     from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+    app.register_blueprint(auth_blueprint,url_prefix ='/auth')
+
+    #Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    #Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    simple.init_app(app)
+
+    #Registering the blueprint
+    from.main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+
+    #Configure UploadSet
+    configure_uploads(app,photos)
+
+    #Will add the views and forms
 
     return app
